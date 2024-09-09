385bca84a73a2f00f46831ad8c20e5003df38d69
pikachu <40382944+pic4xiu@users.noreply.github.com>
2024-09-09 15:12:07 +0800

    Modify log.py to adapt to Windows
    Solve the log path problem on Windows

--- d810/log.py
+++ d810/log.py
@@ -2,21 +2,29 @@ import os
 import shutil
 import logging
 import logging.config
+import platform
 
 LOG_CONFIG_FILENAME = "log.ini"
 LOG_FILENAME = "d810.log"
 Z3_TEST_FILENAME = "z3_check_instructions_substitution.py"
 
+def check_platform():
+    return platform.system() == "Windows"
+
+def normalize_path(path):
+    return path.replace('\\', '/')
 
 def clear_logs(log_dir):
     shutil.rmtree(log_dir, ignore_errors=True)
-
 
 def configure_loggers(log_dir):
     os.makedirs(log_dir, exist_ok=True)
     log_main_file = os.path.join(log_dir, LOG_FILENAME)
     z3_test_file = os.path.join(log_dir, Z3_TEST_FILENAME)
     log_conf_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), LOG_CONFIG_FILENAME)
+    if check_platform():
+        log_main_file=normalize_path(log_main_file)
+        z3_test_file=normalize_path(z3_test_file)
     logging.config.fileConfig(log_conf_file, defaults={"default_log_filename": log_main_file,
                                                        "z3_log_filename": z3_test_file})
     z3_file_logger = logging.getLogger('D810.z3_test')
